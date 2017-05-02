712e3debf0a1ff8a18d515be00e83678302c78cd
sahio813 <olgusahin@gmail.com>
2017-05-03 00:10:19 +0300

    initial commit

---
+++ Assets/ParticleFollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPlayer : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private Vector3 v3Velocity;
+    private ParticleSystem.VelocityOverLifetimeModule particleVelocity;
+    //private ParticleSystem.MinMaxCurve rate = new ParticleSystem.MinMaxCurve();
+
+    void Start () {       
+        particleVelocity = GetComponent<ParticleSystem>().velocityOverLifetime;          
+    }
+
+    void ParticleMovement()
+    {        
+        particleVelocity.x = v3Velocity.x * 0.3f;
+        particleVelocity.y = v3Velocity.y * 0.3f;
+    }
+
+    void Update () {
+        v3Velocity = rb.velocity;
+        ParticleMovement();
+    }
+}
---
+++ Assets/ShadowFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowFollow : MonoBehaviour
+{   
+    private float shadowDist;
+    private float shadowScale;
+    public Transform charPos;
+
+    void Update()
+    {        
+        shadowDist = charPos.transform.localPosition.y;
+        Vector3 tmpPosition = transform.localPosition;
+        tmpPosition.y = -shadowDist - 0.9f;
+        transform.localPosition = tmpPosition;
+        shadowScale = 1f - (shadowDist * 0.3f);        
+        transform.localScale = new Vector3(shadowScale, shadowScale, shadowScale);
+    }
+}
